0f8709abc2c8b9e49191d0089ed360c625b343a1
Pranav Karawale <pranav@karawale.in>
2024-06-16 12:39:50 +0530

    modify api client

--- api/client.ts
+++ api/client.ts
@@ -1,5 +1,7 @@
 import createClient from "openapi-fetch";
 import { paths } from "./schema";
 
-const client = createClient<paths>({ baseUrl: "http://localhost:3000/" });
+const client = createClient<paths>({
+	baseUrl: process.env.NEXT_PUBLIC_API_ENDPOINT ?? "http://localhost:3000/",
+});
 export default client;
